f62b7370435e36ebb9707d8f73c9e366c1f316cd
tusharr08 <tushargupta@Tushars-MacBook-Air.local>
2025-03-03 01:21:27 +0530

    changed data lineage

--- streamlit-app/pages/5_Data_Lineage.py
+++ streamlit-app/pages/5_Data_Lineage.py
@@ -38,4 +38,5 @@ with st.expander('Click to know more about the dbt structure:'):
 st.caption('Go through the models, seeds and tests listed in left pane.')
 st.caption('You can click the blue icon on the bottom-right corner of the page to view the lineage graph of your models. Click on any node to highlight the lineage.')
 
-st.components.v1.iframe("http://nyc-taxi-data-analytics.s3-website-us-west-2.amazonaws.com/#!/overview", height=800, width = 900)
+st.markdown('[View Data Lineage](http://nyc-taxi-data-analytics.s3-website-us-west-2.amazonaws.com/#!/overview)', unsafe_allow_html=True)
+
